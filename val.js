5d23dcd4e19dac705c760f2e43cfb5c85590e4a9
FideosHot <santiagoaldama127@gmail.com>
2024-10-27 15:53:41 -0600

    Add files via upload

---
+++ val.js
@@ -0,0 +1,8 @@
+function mostrarMensaje() {
+    document.getElementById('mensaje').innerText = 'M encantas mucho';
+}
+
+function cambiarColor() {
+    document.body.style.backgroundColor = "#009D71";  // Verde esmeralda
+}
+
